84c51f751d1b5bc22e31e295138921aa90858aea
Torsten <torsten.bergh.moss@gmail.com>
2026-03-15 14:56:05 +0100

    feat: dashboard + cors

--- src/main/java/com/impact/paymentcore/controller/PaymentController.java
+++ src/main/java/com/impact/paymentcore/controller/PaymentController.java
@@ -6,6 +6,7 @@ import com.impact.paymentcore.service.PaymentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/payments")
+@CrossOrigin
 @Tag(name = "Payments", description = "Payment Core API operations")
 public class PaymentController {
 
